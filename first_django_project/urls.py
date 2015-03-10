89771ae23e43a2b9994b6055fcc7714a67db19d6
Pedula <renato.pedula@sr2.uerj.br>
2015-03-10 10:57:07 -0300

    projeto crud django

---
+++ first_django_project/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from contato.models import Contato
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'first_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+   	url(r'^$','contato.views.index'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^',include('contato.urls')),
+
+	
+)
